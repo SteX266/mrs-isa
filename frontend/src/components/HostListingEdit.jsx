5a59426c96b37ac722b867332c70613146e0404b
aleksa-hubgit <aleksa.stev92@gmail.com>
2022-04-20 01:04:35 +0200

    3.6.1.3 Forma za pretragu + pomocne funkcionalnosti

---
+++ frontend/src/components/HostListingEdit.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { Container} from 'react-bootstrap';
+class HostListingEdit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {listingId : ''}
+    }
+    render() {
+        return (
+        <Container>
+        </Container>
+        );
+    }
+}
+ 
+export default HostListingEdit;
