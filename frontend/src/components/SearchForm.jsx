cf55f152790ee82d394eeec85262fb2d52159217
SteX266 <stefan.milosevic.e14@gmail.com>
2022-04-27 05:14:09 +0200

    #3.1.3.2 #3.1.3.3 Filtriranje entiteta pomocu frome za pretragu i linkova

--- frontend/src/components/SearchForm.jsx
+++ frontend/src/components/SearchForm.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React,{Component} from "react";
 import { MDBCol, MDBInput } from "mdbreact";
 
-const SearchForm = () => {
+class SearchForm extends Component{
+    constructor(props){
+        super(props);
+        
+    }
+
+    searchFieldChanged(){
+        
+    }
+
+   render(){ 
   return (
     <MDBCol md="12">
-      <MDBInput hint="Search" type="text" containerClass="active-pink active-pink-2 mt-0 mb-3" />
+      <MDBInput  hint="Search" type="text" containerClass="active-pink active-pink-2 mt-0 mb-3" />
     </MDBCol>
   );
 }
-
+}
 export default SearchForm;
