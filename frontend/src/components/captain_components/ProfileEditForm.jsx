541d9c9a2c9568dabab50dde63c94e8cb3a3f701
aleksa-hubgit <aleksa.stev92@gmail.com>
2022-05-04 15:49:45 +0200

    Sredjene forme za promenu profila i lozinke

--- frontend/src/components/captain_components/ProfileEditForm.jsx
+++ frontend/src/components/captain_components/ProfileEditForm.jsx
@@ -1,43 +1,68 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import LabeledInput from './LabeledInput';
 
-class ProfileEditForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { user: {
+export default function ProfileEditForm (props){
+    const [user, setUser] = useState({
+        name: "Imenko",
+        surname: "Prezimenic",
+        email: "mejl@mail.com",
+        address: "Serbia, Novi Sad, Gogoljeva 11",
+        phoneNumber: "0652988290" 
+    });
+
+    const [backup, setBackup] = useState({
             name: "Imenko",
             surname: "Prezimenic",
             email: "mejl@mail.com",
             address: "Serbia, Novi Sad, Gogoljeva 11",
-            phoneNumber: "0652988290" },
-        backupUser: {
-            name: "Imenko",
-            surname: "Prezimenic",
-            email: "mejl@mail.com",
-            address: "Serbia, Novi Sad, Gogoljeva 11",
-            phoneNumber: "0652988290" 
-        } };
+            phone: "0652988290" 
+    });
+
+    
+    function onCancel() {
+        setUser(backup);
+
     }
-    onCancel = () => {
-        this.setState({user: this.state.backupUser});
+    function onSave() {
+        setBackup(user);
+        saveToBackend();
     }
-    onSave = () => {
-        this.setState({backupUser: this.state.user});
+    function handleChange(event) {
+        setUser(prevUser => {
+            return {
+                ...prevUser,
+                [event.target.name]: event.target.value
+            };
+        });
     }
-    render() { 
+    function saveToBackend() {
+        const requestOptions = {
+            headers: {
+               Accept: 'application/json',
+             'Content-Type': 'application/json',
+             'Access-Control-Allow-Origin': '*',
+               
+               },
+            params:{
+                "user": user
+            }
+   
+        };
+  
+        // axios.get("http://localhost:8080/api/user/change-profile", requestOptions);
+    }
         return (
         <Form>
-            <LabeledInput name='First name' defaultValue={this.state.user.name} controlId='first-name-input'/>
-            <LabeledInput name='Last name'  defaultValue={this.state.user.surname} controlId='last-name-input'/>
-            <LabeledInput name='Email' defaultValue={this.state.user.email} controlId='email-input' type='email'/>
-            <LabeledInput name='Address' defaultValue={this.state.user.address} controlId='address-input'/>
-            <LabeledInput name='Phone number' defaultValue={this.state.user.phoneNumber} controlId='first-name-input' type='tel'/>
+            <LabeledInput text='First name' value={user.name} controlId='first-name-input' name='name' onChange={handleChange}/>
+            <LabeledInput text='Last name'  value={user.surname} controlId='last-name-input' name='surname' onChange={handleChange}/>
+            <LabeledInput text='Email' value={user.email} controlId='email-input' type='mail' name='email' onChange={handleChange}/>
+            <LabeledInput text='Address' value={user.address} controlId='address-input' name='address' onChange={handleChange}/>
+            <LabeledInput text='Phone number' value={user.phoneNumber} controlId='first-name-input' type='tel' name='phone' onChange={handleChange}/>
 
-            <Button style={{marginTop: '16px',marginBottom:'16px', marginRight: '69%'}} variant='outline-dark' onClick={this.onSave}>Save</Button>
-            <Button style={{marginTop: '16px', marginBottom:'16px'}} variant='outline-danger' onClick={this.onCancel}>Cancel</Button>
+            <Button style={{marginTop: '16px',marginBottom:'16px', marginRight: '69%'}} variant='outline-dark' onClick={onSave}>Save</Button>
+            <Button style={{marginTop: '16px', marginBottom:'16px'}} variant='outline-danger' onClick={onCancel}>Cancel</Button>
         </Form>);
-    }
 }
  
-export default ProfileEditForm;
--- frontend/src/components/host_components/PasswordChangeForm.jsx
+++ frontend/src/components/host_components/PasswordChangeForm.jsx
@@ -1,41 +1,64 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+import React, { Component, useState } from 'react';
 import { Alert, Button, Form } from 'react-bootstrap';
 
-class PasswordChangeForm extends Component {
-    state = { oldPassword: "", newPassword: "", successfull: false}
-    onButtonClicked = () => {
-        this.setState({successfull: true});
+export default function PasswordChangeForm() {
+    const [passwords, setPasswords] = useState({old: "", new: "", repeat: ""});
+    const [successfull, setSuccessfull] = useState(false);
+
+    function onButtonClicked() {
+        if (passwords.new === passwords.repeat) {
+            console.log(passwords);
+            sendChangeRequest();
+        } else
+            setSuccessfull(false);
     }
-    render() {
-        return (<Form>
-            <Form.Group>
-                <Form.Label>Old password</Form.Label>
-                <Form.Control type='password'></Form.Control>
+    function sendChangeRequest() {
+        const requestOptions = {
+            headers: {
+               Accept: 'application/json',
+             'Content-Type': 'application/json',
+             'Access-Control-Allow-Origin': '*',
+               
+               },
+            params:{
+                "passwords": passwords
+            }
+        };
+        axios.post("http://localhost:8080/api/user/change-password", requestOptions).then();
 
-                <Form.Label>New password</Form.Label>
-                <Form.Control type='password'></Form.Control>
-
-                <Form.Label>Repeat password</Form.Label>
-                <Form.Control type='password'></Form.Control>
-
-                <Button type='submit' variant='outline-dark' onClick={this.onButtonClicked} style={{marginTop:'16px', marginBottom:'16px'}}>Change password</Button>
-                <PasswordAlert successfull={this.state.successfull}></PasswordAlert>
-            </Form.Group>
-        </Form>);
     }
+    function handleChange(event) {
+        setPasswords(prevPasswords => {
+            return {
+                ...prevPasswords,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+    return (<Form>
+        <Form.Group>
+            <Form.Label>Old password</Form.Label>
+            <Form.Control type='password' name='old' onChange={handleChange}></Form.Control>
+
+            <Form.Label>New password</Form.Label>
+            <Form.Control type='password' name='new' onChange={handleChange}></Form.Control>
+
+            <Form.Label>Repeat password </Form.Label>
+            <Form.Control type='password' name='repeat' onChange={handleChange}></Form.Control>
+
+            <Button type='submit' variant='outline-dark' onClick={onButtonClicked} style={{marginTop:'16px', marginBottom:'16px'}}>Change password</Button>
+            {successfull && <PasswordAlert/>}
+        </Form.Group>
+    </Form>);
 }
 
 class PasswordAlert extends Component {
     render() {
-        const successfull = this.props.successfull;
-        if (successfull) {
-            return(
-                <Alert variant='success'>
-                    <Alert.Heading>Successfully changed password</Alert.Heading>
-                    <p>The password was successfully changed.</p>
-                </Alert>);
-        }
+        return(
+            <Alert variant='success'>
+                <Alert.Heading>Successfully changed password</Alert.Heading>
+                <p>The password was successfully changed.</p>
+            </Alert>);
     }
 }
- 
-export default PasswordChangeForm;
