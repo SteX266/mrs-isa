4fb75ef72159d9f493b4a7b0fbc098f911eed5a6
SteX266 <stefan.milosevic.e14@gmail.com>
2022-05-10 18:17:26 +0200

    #3.2.1.1 Validacija login forme

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,4 +1,71 @@
-const LoginForm = (props) =>  {  
+import React,{useEffect, useState} from 'react';
+import axios from 'axios';
+
+function LoginForm()   {  
+
+  const [errors, setErrors] = useState({email:'', password:''});
+
+
+const[email,setEmail] = useState("");
+const[password, setPassword] = useState("");
+
+const handleSubmit = (event) =>{
+  event.preventDefault();
+  if(validateForm()){
+
+
+    console.log("Uspesan login");
+  }
+  else{
+    console.log("");
+  }
+
+
+}
+
+const validateForm = () => {
+  let currentErrors = errors;
+
+  let valid = true;
+
+  if (email === ""){
+      currentErrors.email = "Email field must be filled";
+      valid = false;
+  }
+  else{
+      currentErrors.email = "";
+  }
+
+  if (password ==="" || password.length < 5){
+    currentErrors.password = "Password must be at least 5 characters long";
+  }
+  else{
+    currentErrors.password = "";
+  }
+
+  setErrors({email:currentErrors.email, password:currentErrors.password});
+  return valid;
+} 
+
+
+const handleChange = (event) => {
+
+  event.preventDefault();
+  const { name, value } = event.target;
+
+  switch (name) {
+  case 'email':
+      setEmail(value);
+      break;
+  case 'password':
+      setPassword(value);
+      break;
+    default:
+      break;
+  }
+
+}
+
     return <>
 
 <div class="album py-5 bg-light">
@@ -7,20 +74,24 @@ const LoginForm = (props) =>  {
 
 
 
+
   <div class="mb-6">
-    <label for="exampleInputEmail1" class="form-label">Email adresa</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">Vaša email adresa nikad neće biti javno vidljiva niti deljena ni sa kim.</div>
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input onChange={handleChange} name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div   id="emailHelp" class="form-text">Your email address will never be shared with anyone.</div>
+    {errors.email.length > 0 && <span className='error'>{errors.email}</span>}
   </div>
   <div class="mb-6">
-    <label for="exampleInputPassword1" class="form-label">Lozinka</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input onChange={handleChange} name="password" type="password" class="form-control" id="exampleInputPassword1"/>
+    {errors.password.length > 0 && <span className='error'>{errors.password}</span>}
   </div>
 
-  <button type="submit" class="btn btn-primary">Prijavi se</button>
+  <button onClick={handleSubmit} type="submit" class="btn btn-primary">Login</button>
 </form>
 </div>
 </div>
 </>}
 
 export default LoginForm;
+
