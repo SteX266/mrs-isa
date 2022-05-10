c9eab322cb28ac435bf2c0f7191afa932a578b81
SteX266 <stefan.milosevic.e14@gmail.com>
2022-05-11 01:16:34 +0200

    Dodat login do autentifikacije

---
+++ tim22/src/main/java/com/mrsisa/tim22/dto/UserTokenState.java
@@ -0,0 +1,19 @@
+package com.mrsisa.tim22.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserTokenState {
+
+    private String accessToken;
+    private Long expiresIn;
+
+
+
+}
