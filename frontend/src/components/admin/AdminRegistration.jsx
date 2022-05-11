5c30c8e90920001b15182cc72ec69dcf28942c85
Vanja Šerfeze <serfezev@gmail.com>
2022-05-11 02:32:03 +0200

    #3.11.2.2 provera na serveru

---
+++ frontend/src/components/admin/AdminRegistration.jsx
@@ -0,0 +1,283 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../../style/Errors.css";
+
+export default function AdminRegistration() {
+  const [errors, setErrors] = useState({
+    name: "",
+    surname: "",
+    phoneNumber: "",
+    addressLine: "",
+    email: "",
+    repeatedPassword: "",
+    password: "",
+  });
+
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [addressLine, setAddressLine] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatedPassword, setRepeatedPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email,
+          name,
+          surname,
+          phoneNumber,
+          addressLine,
+          password,
+        }),
+      };
+
+      fetch("http://localhost:8080/api/user/signup", requestOptions).then(
+        async (response) => {
+          const data = await response.json();
+
+          if (data.email !== null) {
+            console.log("Uspesna registracija!");
+          } else {
+            console.log("Neuspesna registracija!");
+          }
+        }
+      );
+    }
+  };
+
+  const emailValidation = (email) => {
+    const regex =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return !email || regex.test(email) === false;
+  };
+
+  const validateForm = () => {
+    let currentErrors = errors;
+
+    let valid = true;
+
+    if (name === "") {
+      currentErrors.name = "Name field must be filled";
+      valid = false;
+    } else {
+      currentErrors.name = "";
+    }
+    if (surname === "") {
+      currentErrors.surname = "Surname field must be filled";
+
+      valid = false;
+    } else {
+      currentErrors.surname = "";
+    }
+    if (phoneNumber === "" || phoneNumber.length < 9) {
+      currentErrors.phoneNumber =
+        "Phone number must contain at least 9 characters";
+
+      valid = false;
+    } else {
+      currentErrors.phoneNumber = "";
+    }
+    if (addressLine === "") {
+      currentErrors.addressLine = "Address line field is required!";
+      valid = false;
+    } else {
+      currentErrors.addressLine = "";
+    }
+    if (emailValidation(email)) {
+      currentErrors.email = "Email is not valid!";
+    } else {
+      currentErrors.email = "";
+    }
+    if (password === "" || password.length < 5) {
+      currentErrors.password = "Password must be at least 5 characters long";
+    } else {
+      currentErrors.password = "";
+    }
+
+    if (password !== repeatedPassword) {
+      currentErrors.repeatedPassword = "Passwords do not match";
+    } else {
+      currentErrors.repeatedPassword = "";
+    }
+
+    setErrors({
+      name: currentErrors.name,
+      surname: currentErrors.surname,
+      phoneNumber: currentErrors.phoneNumber,
+      addressLine: currentErrors.addressLine,
+      email: currentErrors.email,
+      password: currentErrors.password,
+      repeatedPassword: currentErrors.repeatedPassword,
+    });
+    return valid;
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "surname":
+        setSurname(value);
+        break;
+      case "phoneNumber":
+        setPhoneNumber(value);
+        break;
+      case "addressLine":
+        setAddressLine(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "repeatedPassword":
+        setRepeatedPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <>
+      <div class="album py-5 bg-light">
+        <div class="container">
+          <form>
+            <div class="mb-6">
+              <label for="exampleInputEmail1" class="form-label">
+                Email address
+              </label>
+              <input
+                name="email"
+                onChange={handleChange}
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" class="form-text">
+                Your email address will not be shared with anyone.
+              </div>
+              {errors.email.length > 0 && (
+                <span className="error">{errors.email}</span>
+              )}
+            </div>
+            <div class="mb-6">
+              <label for="exampleInputPassword1" class="form-label">
+                Password
+              </label>
+              <input
+                name="password"
+                onChange={handleChange}
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+              />
+              {errors.password.length > 0 && (
+                <span className="error">{errors.password}</span>
+              )}
+            </div>
+            <div class="mb-6">
+              <label for="exampleInputPassword1" class="form-label">
+                Repeat passwrod
+              </label>
+              <input
+                name="repeatedPassword"
+                onChange={handleChange}
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+              />
+              {errors.repeatedPassword.length > 0 && (
+                <span className="error">{errors.repeatedPassword}</span>
+              )}
+            </div>
+            <div class="mb-6">
+              <label for="exampleInputEmail1" class="form-label">
+                Name
+              </label>
+              <input
+                name="name"
+                onChange={handleChange}
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              {errors.name.length > 0 && (
+                <span className="error">{errors.name}</span>
+              )}
+            </div>
+            <div class="mb-6">
+              <label for="exampleInputEmail1" class="form-label">
+                Surname
+              </label>
+              <input
+                name="surname"
+                onChange={handleChange}
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              {errors.surname.length > 0 && (
+                <span className="error">{errors.surname}</span>
+              )}
+            </div>
+            <div class="mb-6">
+              <label for="exampleInputEmail1" class="form-label">
+                Home address
+              </label>
+              <input
+                name="addressLine"
+                onChange={handleChange}
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              {errors.addressLine.length > 0 && (
+                <span className="error">{errors.addressLine}</span>
+              )}
+            </div>
+            <div class="mb-6">
+              <label for="exampleInputEmail1" class="form-label">
+                Phone number
+              </label>
+              <input
+                name="phoneNumber"
+                onChange={handleChange}
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              {errors.phoneNumber.length > 0 && (
+                <span className="error">{errors.phoneNumber}</span>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              class="btn btn-primary"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/src/components/admin/AdminRouter.jsx
+++ frontend/src/components/admin/AdminRouter.jsx
@@ -3,6 +3,7 @@ import AdminNavbar from "./AdminNavbar";
 import RegistrationRequest from "./RegistrationRequest";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminProfile from "./AdminProfile";
+import AdminRegistration from "./AdminRegistration";
 
 export default function AdminRouter() {
   return (
@@ -15,6 +16,8 @@ export default function AdminRouter() {
           element={<RegistrationRequest />}
         />
         <Route path="/admin/profile" element={<AdminProfile />} />
+        <Route path="/admin/registerAdmin" element={<AdminRegistration />} />
+        /admin/registerAdmin`
       </Routes>
     </BrowserRouter>
   );
