75fe0e470a3ba067e9bcfb744c3fc2286e810fae
Vanja Šerfeze <serfezev@gmail.com>
2022-05-16 18:44:56 +0200

    3.11.4.2 Dodavanje dugmeta za brisanje

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,6 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap";
 import "./App.css";
-import HomePageUnregistered from "./components/HomePageUnregistered";
-import BussinessUserRouter from "./components/business/BusinessUserRouter";
-
-import InstructorPage from "./components/InstructorPage";
-import BusinesssUserRouter from "./components/business/BusinessUserRouter";
 import AdminRouter from "./components/admin/AdminRouter";
 
 function App() {
