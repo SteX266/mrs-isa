8a90364c3b4e6405ce996528488aba17e2fed588
Vanja Šerfeze <serfezev@gmail.com>
2022-06-17 15:49:29 +0200

    popravljen bag na kalendaru

--- frontend/src/components/BigCalendar.jsx
+++ frontend/src/components/BigCalendar.jsx
@@ -1,29 +1,69 @@
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
 import React, { useState, useEffect } from "react";
-import { Calendar } from "react-big-calendar";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "react-datepicker/dist/react-datepicker.css";
 import axios from "axios";
 import { useParams } from "react-router";
 
-import { momentLocalizer } from "react-big-calendar";
-import moment from "moment";
-import "moment-timezone";
-
-moment.tz.setDefault("Europe/Belgrade");
-const localizer = momentLocalizer(moment);
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 export default function BigCalendar() {
   const params = useParams();
 
   const [allEvents, setAllEvents] = useState([]);
-  const [availabilityPeriods, setAvailabilityPeriods] = useState([]);
 
   useEffect(() => {
     getReservations(params.id);
-    getAvailabilityPeriods(params.id);
+    getEntityAvailabilityPeriods(params.id);
+    getEntityPromos(params.id);
   }, []);
 
-  function getAvailabilityPeriods(entityId) {
+  function getReservations(entityId) {
+    const token = JSON.parse(localStorage.getItem("userToken"));
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: "Bearer " + token.accessToken,
+      },
+      params: {
+        id: entityId,
+      },
+    };
+
+    axios
+      .get(
+        "http://localhost:8080/reservation/getEntityReservations",
+        requestOptions
+      )
+      .then((res) => {
+        let events = [];
+        res.data.forEach((element) => {
+          let startDate = new Date(element.startDate);
+          let endDate = new Date(element.endDate);
+          events.push({
+            title: element.client,
+            start: startDate,
+            end: endDate,
+          });
+        });
+        setAllEvents(events);
+      });
+  }
+  function getEntityAvailabilityPeriods(entityId) {
     const token = JSON.parse(localStorage.getItem("userToken"));
     const requestOptions = {
       method: "GET",
@@ -45,19 +85,27 @@ export default function BigCalendar() {
         let events = [];
         res.data.forEach((element) => {
           let startDate = new Date(element.dateFrom);
+          if (startDate.getTime() < new Date().getTime())
+            startDate = new Date();
           let endDate = new Date(element.dateTo);
           events.push({
-            allDay: false,
-            title: "Available",
+            title: "available",
             start: startDate,
             end: endDate,
           });
+          setAllEvents((prevState) => [
+            ...prevState,
+            {
+              title: "available",
+              start: startDate,
+              end: endDate,
+            },
+          ]);
         });
-        setAllEvents(events);
       });
   }
 
-  function getReservations(entityId) {
+  function getEntityPromos(entityId) {
     const token = JSON.parse(localStorage.getItem("userToken"));
     const requestOptions = {
       method: "GET",
@@ -71,26 +119,47 @@ export default function BigCalendar() {
     };
 
     axios
-      .get(
-        "http://localhost:8080/reservation/getEntityReservations",
-        requestOptions
-      )
+      .get("http://localhost:8080/promo/getEntityPromos", requestOptions)
       .then((res) => {
         let events = [];
         res.data.forEach((element) => {
-          console.log(element);
-          let startDate = new Date(element.startDate);
-          let endDate = new Date(element.endDate);
+          let startDate = new Date(element.dateFrom);
+          if (startDate.getTime() < new Date().getTime())
+            startDate = new Date();
+          let endDate = new Date(element.dateTo);
           events.push({
-            allDay: false,
-            title: element.client,
+            title: "promo",
             start: startDate,
             end: endDate,
           });
+          setAllEvents((prevState) => [
+            ...prevState,
+            {
+              title: "PROMO",
+              start: startDate,
+              end: endDate,
+            },
+          ]);
         });
-        setAllEvents(events);
       });
   }
+
+  const eventPropGetter = React.useCallback(
+    (event) => ({
+      ...(!event.title.includes("available") && {
+        style: {
+          backgroundColor: "#6ac454",
+        },
+      }),
+      ...(event.title.includes("PROMO") && {
+        style: {
+          backgroundColor: "#73021e",
+          color: "white",
+        },
+      }),
+    }),
+    []
+  );
 
   return (
     <div className="App">
@@ -99,7 +168,8 @@ export default function BigCalendar() {
         events={allEvents}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 500, margin: "50px", backgroundColor: "white" }}
+        style={{ height: 800, margin: "50px", backgroundColor: "white" }}
+        eventPropGetter={eventPropGetter}
       />
     </div>
   );
